2ef82e374ade62c664d34c07cabb176e8f62596a
Glitch (glitch-hello-node) <none>
2022-02-17 09:52:09 +0000

    ⚡️☔️ Checkpoint ./package.json:31128804/136 ./server.js:31128804/774

--- server.js
+++ server.js
@@ -13,20 +13,36 @@ fastify.register(require("fastify-static"), {
   prefix: "/" // optional: default '/'
 });
 
+fastify.register(require("fastify-sse"), (err) => {
+    if (err) {
+      throw err;
+    }
+});
+
 const seo = require("./src/seo.json");
 if (seo.url === "glitch-default") {
   seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
 }
 
 const people = []
+const cbs = []
 
 fastify.post("/p", (req, res) => {
   const {name} = req.body
+  if (!name) 
+    name = "Guest";
+  
+  res.sse("people", people)
+  
   people.push(name)
+  
+  const cb = () => res.sse("people", people)
+  cbs.push(cb)
 })
 
 fastify.listen(process.env.PORT, '0.0.0.0', function(err, address) {
   if (err) {
+    console.log("death")
     console.error(err);
     process.exit(1);
   }
