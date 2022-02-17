026ee125eb229ad42d268705d024df863f8e7814
Glitch (glitch-hello-node) <none>
2022-02-17 10:15:10 +0000

    😋🚄 Checkpoint ./server.js:31128804/2638 ./public/index.html:31128804/4371 ./package.json:31128804/2182

--- server.js
+++ server.js
@@ -1,46 +1,13 @@
-/**
-* This is the main Node.js server script for your project
-* Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
-*/
-const path = require("path");
-const {FastifySSEPlugin} = require("fastify-sse-v2")
+const express = require('express')
+const app = express()
+const port = 3000
 
-const fastify = require("fastify")({
-  logger: false
-});
+app.use(express.static('public'))
 
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
-});
-
-fastify.register(FastifySSEPlugin)
-
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
-
-const people = []
-const cbs = []
-
-fastify.post("/p", (req, res) => {
-  const {name = "Guest"} = req.body ?? {name: ""}
-  
-  for (const fn of cbs) {
-    fn()
-  }
-  
-  people.push(name)
-  
-  const cb = () => res.sse("people", people)
-  cbs.push(cb)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-fastify.listen(process.env.PORT, '0.0.0.0', function(err, address) {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Your app is listening on ${address}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
