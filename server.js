775a05b5dd32b33dc53c5e369688f6615305b144
Glitch (glitch-hello-node) <none>
2022-02-17 16:36:01 +0000

    👐🐘 Checkpoint ./public/index.html:31128804/34429 ./server.js:31128804/4317

--- server.js
+++ server.js
@@ -1,99 +1,127 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-app.use(express.static('public'))
-app.use(bodyParser.json())
+app.use(express.static("public"));
+app.use(bodyParser.json());
 
-const people = {}
-const reslist = []
-const messages = []
-const votes = [{
-  hour: 17,
-  votes: 1
-}]
+const people = {};
+const reslist = [];
+const messages = [];
+const votes = [
+  {
+    hour: 17,
+    votes: 1,
+  },
+];
 
 function event() {
   for (const res of reslist) {
-    if (res.writableEnded) continue
-    res.write(`data: ${JSON.stringify({type: 'people', people: Object.values(people)})}\n\n`)
+    if (res.writableEnded) continue;
+    res.write(
+      `data: ${JSON.stringify({
+        type: "people",
+        people: Object.values(people),
+      })}\n\n`
+    );
   }
 }
 
 function chatEvent(message, id) {
   for (const res of reslist) {
-    if (res.writableEnded) continue
-    res.write(`data: ${JSON.stringify({type: 'chat', message: message, person: people[id]})}\n\n`)
+    if (res.writableEnded) continue;
+    res.write(
+      `data: ${JSON.stringify({
+        type: "chat",
+        message: message,
+        person: people[id],
+      })}\n\n`
+    );
   }
 }
 
-app.get('/e', async function(req, res) {
+app.get("/e", async function (req, res) {
   res.set({
-    'Cache-Control': 'no-cache',
-    'Content-Type': 'text/event-stream',
-    'Connection': 'keep-alive'
+    "Cache-Control": "no-cache",
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
   });
   res.flushHeaders();
 
-  let {name = "Guest", id} = req.query
+  let { name = "Guest", id } = req.query;
   if (!id) {
     res.end();
-    return
+    return;
   }
-  
-  if (name.length === 0) name = "Guest"
-  
-  const cbsidx = reslist.length
-  people[id] = name
-  
-  reslist.push(res)
 
-  req.on('close', () => {
-    delete people[id]
-    event()
-  })
+  if (name.length === 0) name = "Guest";
 
-  event()
-  
-  res.write('retry: 1000\n\n');
+  const cbsidx = reslist.length;
+  people[id] = name;
+
+  reslist.push(res);
+
+  req.on("close", () => {
+    delete people[id];
+    event();
+  });
+
+  event();
+
+  res.write("retry: 1000\n\n");
 });
 
-app.post('/name', function(req, res) {
-  const {name = "Guest", id} = req.body
+app.post("/name", function (req, res) {
+  const { name = "Guest", id } = req.body;
   if (!id || !people[id]) {
     res.end();
     return;
   }
-  
+
   people[id] = name.length === 0 ? "Guest" : name;
-  event()
-  res.end()
-})
+  event();
+  res.end();
+});
 
-app.get('/chat', function(req, res) {
-  res.send(
-    JSON.stringify(messages.map(m => ({message: m.message, person: people[m.id] || "Guest"})))
-  )
-})
-
-app.get("/votes", function(req, res) {
-  res.send(JSON.stringify(votes))
-})
-
-app.post('/chat', function(req, res) {
-  const {message, id} = req.body
-  if (!message || !id) {
+app.post("/vote", function (req, res) {
+  const {vote} = req.body
+  if (!vote) {
     res.end()
     return
   }
   
-  messages.push({message, id})
-  chatEvent(message, id)
-  res.end()
-})
+  
+});
+
+app.get("/chat", function (req, res) {
+  res.send(
+    JSON.stringify(
+      messages.map((m) => ({
+        message: m.message,
+        person: people[m.id] || "Guest",
+      }))
+    )
+  );
+});
+
+app.get("/votes", function (req, res) {
+  res.send(JSON.stringify(votes));
+});
+
+app.post("/chat", function (req, res) {
+  const { message, id } = req.body;
+  if (!message || !id) {
+    res.end();
+    return;
+  }
+
+  messages.push({ message, id });
+  chatEvent(message, id);
+  res.end();
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
